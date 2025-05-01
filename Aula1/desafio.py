ea8a8b8ae6d0aab381a452cd6fd3e957e88c61fa
Bernardofigueira <bernardobfzin@gmail.com>
2025-05-01 16:31:40 -0300

    Add files via upload

---
+++ Aula1/desafio.py
@@ -0,0 +1,14 @@
+
+time = input('Qual é o seu time? ')#A função input()captura algum dado informado pelo usuario
+print('O seu time é',time)
+nome = 'Ronaldo'
+#Formas de saída da informação
+
+    #Primeira forma
+print('Primeira forma:',time, nome)
+
+    #Segunda Forma
+print(f"Segunda forma:{time} {nome}")
+
+    #Terceira Forma
+print('Terceira forma {}'.format(time,nome))
---
+++ Aula1/sla.py
@@ -0,0 +1,6 @@
+print('Hello World!!!')
+idade= int(input('Qual é a sua idade?'))
+if idade>=18: 
+    print('Maior de idade')
+else: 
+   print('Menor de idade')
